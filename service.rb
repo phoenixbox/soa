3aff1fb48d985c634bd5c3356e5e0ee9eba3dc3b
Shane Rogers <rogerssh@tcd.ie>
2013-06-09 15:56:34 -0600

    put the user updates

--- service.rb
+++ service.rb
@@ -40,6 +40,7 @@ get '/api/v1/users/:name' do
   end
 end
 
+# create a user
 post '/api/v1/users' do
   begin
     user = User.create(JSON.parse(request.body.read))
@@ -50,5 +51,22 @@ post '/api/v1/users' do
     end
   rescue => e
     error 400, e.message.to_json
+  end
+end
+
+put '/api/v1/users/:name' do
+  user = User.find_by_name(params[:name])
+  if user
+    begin
+      if user.update_attributes(JSON.parse(request.body.read))
+        user.to_json
+      else
+        error 400, user.errors.to_json
+      end
+    rescue => e
+      error 400, e.message.to_json
+    end
+  else
+    error 404, {:error => "user not found"}.to_json
   end
 end
